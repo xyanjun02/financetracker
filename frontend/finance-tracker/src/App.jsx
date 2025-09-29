079026bbbb1d148bd6f8d6479c6ae9c907c2c7d8
xyanjun02 <yanjunxie611@gmail.com>
2025-09-29 14:47:50 +0800

    set-up project structure

---
+++ frontend/finance-tracker/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
